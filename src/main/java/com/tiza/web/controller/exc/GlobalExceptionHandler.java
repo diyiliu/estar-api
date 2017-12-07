98c4a4a586292c363269589639e11c1a65198769
DIYILIU <572772828@qq.com>
2017-12-07 11:17:18 +0800

    添加异常处理

---
+++ src/main/java/com/tiza/web/controller/exc/GlobalExceptionHandler.java
@@ -0,0 +1,35 @@
+package com.tiza.web.controller.exc;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.servlet.NoHandlerFoundException;
+
+import javax.servlet.http.HttpServletRequest;
+
+/**
+ * Description: GlobalExceptionHandler
+ * Author: DIYILIU
+ * Update: 2017-12-07 10:28
+ */
+
+@RestController
+@ControllerAdvice
+public class GlobalExceptionHandler {
+
+    @ExceptionHandler(NoHandlerFoundException.class)
+    @ResponseStatus(HttpStatus.NOT_FOUND)
+    public String noHandlerFound() {
+
+        System.out.println(404);
+
+        return "404";
+    }
+
+    @ExceptionHandler(Exception.class)
+    public String defaultErrorHandler(HttpServletRequest request, Exception e) throws Exception {
+
+        System.out.println(e);
+
+        return "error";
+    }
+}
--- src/test/java/hello/HelloControllerTest.java
+++ src/test/java/hello/HelloControllerTest.java
@@ -13,6 +13,7 @@ import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.http.MediaType;
 import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.ResultActions;
 import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
 
 /**
@@ -35,4 +36,11 @@ public class HelloControllerTest {
                 .andExpect(status().isOk())
                 .andExpect(content().string(equalTo("Greetings from Spring Boot!")));
     }
+
+    @Test
+    public void getException() throws Exception {
+        ResultActions result = mvc.perform(MockMvcRequestBuilders.get("/test").accept(MediaType.APPLICATION_JSON));
+        result.andExpect(content().string(equalTo("404")));
+    }
+
 }
